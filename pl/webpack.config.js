fa390db4e9fa3a86c3db087a8898ccb3a6dc82a6
Jose Lombana <jlombana@endava.com>
2017-04-06 16:22:39 -0500

    add linters react redux and npm scripts

--- pl/webpack.config.js
+++ pl/webpack.config.js
@@ -4,17 +4,27 @@ const config = {
   entry: './src/index.js',
   devtool: 'eval',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
       {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
       }
     ]
+  },
+  devServer: {
+    publicPath: '/public/',
+    historyApiFallback: true
   },
   resolve: {
     extensions: ['.js', '.json']
